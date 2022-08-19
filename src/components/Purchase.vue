6ce29ed1d395e68fee3b4d0ed806f1861f4abfec
King <tshimologokingmabena@gmail.com>
2022-08-19 11:23:26 +0200

    first commit

---
+++ src/components/Purchase.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="container">
+        <div class="head">
+            <h1>FILL UP AND GO</h1>
+            <h2>Step 1: Type how litres you will pour</h2>
+            <h2>Step 2: Get the price</h2>
+            <h2>Step 3: Take your QR Code to any refilling station and let them scan your purchase and drive away with a full tank</h2>
+        </div>
+        <div class="body">
+            <form @submit.prevent="petrolPrice">
+                <label for="price">How many litres are you going to pour?</label>
+                <input type="number" id="petrol">
+                <button type="submit">Submit</button>
+                <div>
+                    <p>The Price of your fuel is: R</p>
+                    <p id="price"></p>
+                </div>
+            </form>
+        </div>
+        <div class="qrCode">
+            <div v-if="showModal">
+                <div class="backdrop">
+                <div class="modal-content">
+                    <img src="../assets/QR.png" alt="">
+                    <button class="closeButton" @click="toggleModal">
+                    Close Modal
+                    </button>
+                </div>
+                </div>
+            </div>
+            <button class="openButton" @click="toggleModal" id="qrCode">
+                QR CODE
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            showModal: false,
+        }
+    },
+    methods: {
+        toggleModal() {
+      this.showModal = !this.showModal;
+    }
+    }
+}
+</script>
+
+<style scoped>
+*{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+.qrCode{
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-content: center;
+}
+
+.openButton{
+    width: 25%;
+    padding: 5px;
+}
+.backdrop {
+  height: 100%;
+}
+.modal-content {
+  background-color: rgb(0, 0, 0);
+  padding: 50px;
+  height: 500px;
+  width: 500px;
+  border-radius: 25px;
+}
+img{
+    width: 400px;
+    height: 400px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,90 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="home">
+    <!-- <div class="details">
+      <div class="card" id="earn">
+        <div class="icon">
+          <font-awesome-icon icon="wallet" />
+        </div>
+        <div class="text">
+          <h2>Earn Points</h2>
+          <p>
+            Earn points for everytime you use your qr code and you are partnered
+            with the following providers
+          </p>
+        </div>
+      </div>
+      <div class="card" id="track">
+        <div class="icon">
+          <font-awesome-icon icon="chart-column" />
+        </div>
+        <div class="text">
+          <h2>Track Spending</h2>
+          <p>
+            Track your spending everytime you pour a litre and how much you pay
+            to add to your budget plan.
+          </p>
+        </div>
+      </div>
+      <div class="card" id="payments">
+        <div class="icon">
+          <font-awesome-icon icon="calendar-days" />
+        </div>
+        <div class="text">
+          <h2>Payments</h2>
+          <p>
+            Set your payment plan so that you can still pour and get to your
+            destination.
+          </p>
+          <p>Set Up Your Account Today.</p>
+        </div>
+      </div>
+    </div> -->
+    <div id="homeLanding2">
+      <div class="topHalf">
+        <h2>
+           FILL UP YOUR TANK<br />
+          EARN AND PAY LATER
+        </h2>
+      </div>
+      <div class="below"></div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap');
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+#home {
+  background: url(../assets/petrol_station.jpg);
+  height: 100vh;
+  background-size: contain;
+  overflow: hidden !important;
+}
+
+
+.topHalf {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  text-align: center;
+  font-size: 35px;
+  font-family: 'Roboto Condensed', sans-serif;
+}
+
+.topHalf h2 {
+  color: white;
+  text-shadow: -1px -1px 0 rgb(0, 0, 0), 1px -1px 0 rgb(0, 0, 0),
+    -1px 1px 0 rgb(0, 0, 0), 1px 1px 0 rgb(0, 0, 0);
+}
+</style>
